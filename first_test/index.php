39638f8f7d83913ba768267b0cce8ed9602d4cf4
ali <60451029+mahmoudkhani80@users.noreply.github.com>
2022-11-27 22:17:09 +0330

    Add files via upload

---
+++ first_test/index.php
@@ -0,0 +1,65 @@
+<?php  include 'inc/header.php'  ?>
+
+  <?php $name = $email = $body = ""   ;
+        $nameErr = $emailErr = $bodyErr = "";
+
+        if(isset($_POST['submit'])){
+          if(empty($nameErr)){
+            $name = filter_input(INPUT_POST , 'name' , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+          } else {
+            $nameErr = 'name required';
+          }
+          if(empty($emailErr)){
+            $email = filter_input(INPUT_POST , 'email' , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+          } else {
+            $emailErr = 'email required';
+          }
+          if(empty($bodyErr)){
+            $body = filter_input(INPUT_POST , 'body' , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+          } else {
+            $bodyErr = 'feedback required';
+          }
+          if(empty($nameErr) && empty($emailErr) && empty($bodyErr)){
+            $sql = "INSERT INTO feedback (name,email,body) VALUES('$name','$email','$body')";
+            if(mysqli_query($conn ,$sql)){
+              header('location: feedback.php');
+            } else {
+              echo 'error' . mysqli_error($conn);
+            }
+          }
+        }
+  
+  ?>
+
+
+    <img src="/feedback/img/logo.png" class="w-25 mb-3" alt="">
+    <h2>Feedback</h2>
+    <p class="lead text-center">Leave feedback for Me!</p>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="mt-4 w-75">
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control <?php echo $nameErr ? 'is-invalid' : null;  ?>" id="name" name="name" placeholder="Enter your name">
+        <div class="invalid-feedback">
+          <?php echo $nameErr; ?>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control <?php echo $emailErr ? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email">
+        <div class="invalid-feedback">
+          <?php echo $emailErr?>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="body" class="form-label">Feedback</label>
+        <textarea class="form-control <?php echo $bodyErr ? 'is-invalid' : null;?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <div class="invalid-feedback">
+          <?php echo $bodyErr?>
+        </div>
+      </div>
+      <div class="mb-3">
+        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+      </div>
+    </form>
+
+<?php  include 'inc/footer.php' ?>
